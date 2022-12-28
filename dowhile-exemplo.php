a9ade79e8b9460df430819769e691faecf8f170c
Sara Mota <saraamotalima@gmail.com>
2022-12-28 19:04:03 -0300

    dowhile

---
+++ dowhile-exemplo.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Bubble Sort</title>
+</head>
+
+<body>
+<pre>
+<?php
+
+$frutas = ["banana","maçã","pêra","uva"];
+
+$contador = count($frutas);
+
+$i = 2;
+
+do {
+  echo $frutas[$i] . "\n";
+  $i++;
+} while ($i < $contador);
+
+
+?>
+</pre>
+</body>
+
+</html>

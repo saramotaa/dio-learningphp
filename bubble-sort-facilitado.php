29c2000611daa8517fdfc742080d55fc58eac7c1
Sara Mota <saraamotalima@gmail.com>
2022-12-28 17:45:42 -0300

    bubble sort facilitado

---
+++ bubble-sort-facilitado.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Bubble Sort</title>
+</head>
+
+<body>
+<pre>
+<?php
+
+echo "ANTES<br>";
+
+$arr = [1,5,3,0,2,6,4];
+print_r($arr);
+
+echo "DEPOIS<br>";
+sort($arr);
+print_r($arr);
+
+?>
+
+</pre>
+</body>
+
+</html>

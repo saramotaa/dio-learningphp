0b691e10952b9f3bf68e3605519f4e5e40187612
Sara Mota <saraamotalima@gmail.com>
2022-12-28 15:30:01 -0300

    for exemplos

---
+++ for-exemplos.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "PARA 0 ATÉ 10 FAÇA\n";
+for ($n=0; $n<=10; $n+=1) {
+  echo "\n $n";
+}
+echo " || \n";
+
+
+echo "PARA 100 ATÉ 0 DE 10 EM 10 FAÇA\n";
+for ($n=100; $n>=0; $n-=10) {
+  echo "\n $n";
+}
+echo " || \n ";
+
+
+echo "PARA 20 ATÉ 0 DE 2 EM 2 FAÇA\n";
+for ($n=20; $n >= 0; $n-=2) { 
+  echo "\n $n";
+}
+
+?>
